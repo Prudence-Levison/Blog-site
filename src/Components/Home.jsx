899531c2bcb577f1fccc4e986e0d5ec11f450f8f
Prudence Levison <uzoekwelevisoncgmail.com>
2024-04-08 09:35:36 +0100

    I worked on the postpage, not yet complete but would update with time

---
+++ src/Components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Postpage from "./Postpage";
+function Home (){
+    return<>
+     <div>A SIMPLE BLOG SITE</div>
+     <Postpage />
+    
+    </>
+};
+export default Home;
